18153418a75c9202d16b53875b0a8226b9e52a4f
Coadman <tylercoad55@gmail.com>
2024-06-10 19:25:05 -0500

    selecting_data

---
+++ Data_Validation.sql
@@ -0,0 +1,11 @@
+SELECT * FROM crowdfunding_schema.category
+ORDER BY category_id ASC LIMIT 100;
+
+SELECT * FROM crowdfunding_schema.contacts
+ORDER BY contact_id ASC LIMIT 100;
+
+SELECT * FROM crowdfunding_schema.crowdfunding_campaign
+ORDER BY cf_id ASC LIMIT 100;
+
+SELECT * FROM crowdfunding_schema.subcategory
+ORDER BY subcategory_id ASC LIMIT 100;
